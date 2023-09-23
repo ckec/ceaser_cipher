85385fef05d3660336846a9bd4de256738c72915
Kecolol <cvejka.kecman@gmail.com>
2023-09-23 15:16:41 +0200

    add one more way to solve the problem

--- code.rb
+++ code.rb
@@ -24,3 +24,22 @@ def ceaser_cipher(string, shift)
 end
 
 ceaser_cipher("What a string!", 5)
+
+def caesar_cipher_with_modulo(string, shift)
+  result = ""
+  string.each_char do |char|
+    if char.match?(/[a-z]/)
+      char = ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr 
+      # char current ordinal value - ordinal value of first char in range + shift we want to make
+      # % 26 makes sure we stay in the range because 
+      # ex. (120 - 97 + 5 = 28) % 26 + 'a'.ord = 28 % 26 + 97 = 99)
+      # 99 in ordinal when converted back to string with .chr is 'c'
+    elsif char.match?(/[A-Z]/)
+      char = ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+    end
+    result += char
+  end
+  puts result
+end
+
+caesar_cipher_with_modulo("dog", 5) 
